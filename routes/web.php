e8cab8c6c60d8ac7206519540d5ee1fef59cf771
Your Name <officialzebby143@gmail.com>
2026-06-12 20:37:00 +0300

    m

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(['message' => 'Laravel is working!']);
 });
+
+Route::get('/test-didit', function () {
+    return [
+        'api_key_exists' => !empty(config('didit.api_key')),
+        'api_key_length' => strlen(config('didit.api_key') ?? ''),
+        'base_url' => config('didit.base_url'),
+    ];
+});
